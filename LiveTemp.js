508e3ebf6e3aa72ea6099c26d928e1e0ea81425b
Kwonunn <kwonunnx@gmail.com>
2020-11-13 10:25:56 +0100

    Fix js

--- LiveTemp.js
+++ LiveTemp.js
@@ -1,13 +1,13 @@
-window.onload = temperatureUpdater;
-var testTemp = 5;
+window.onload = temperatureUpdater; // Start the temperatureupdater function once upon load of the page
 
 function temperatureUpdater() {
     updateTemperature();
-    window.setInterval(updateTemperature, 5000);
+    window.setInterval(updateTemperature, 5000); // automatically execute the update function every 5 seconds
 }
 
 function updateTemperature() {
     let temperatureValue = document.getElementById("TemperatureValue");
+    
     let fetchedValue = 100;
     
     temperatureValue.innerHTML = fetchedValue;
