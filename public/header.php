9a58fe82b3dded994f3179a6b1079534b4991af1
Ward Pieters <ward@wardpieters.nl>
2020-11-15 14:26:28 +0100

    fix: viewport

--- public/header.php
+++ public/header.php
@@ -4,8 +4,10 @@ include 'config.php';
 <!DOCTYPE html>
 <html lang="en" style="background-color: rgb(35, 35, 47);">
 <head>
-    <meta charset="ISO-8859-1">
+    <meta charset="UTF-8">
     <title>NerdyGadgets</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <!-- CSS -->
     <link rel="stylesheet" href="public/css/style.css" type="text/css">
