7d1c4710c22a7c80bd2ad9024ceb2862430049f9
Ward Pieters <ward@wardpieters.nl>
2020-11-18 16:12:26 +0100

    set footer position

--- public/footer.php
+++ public/footer.php
@@ -1,6 +1,6 @@
 </div>
 </div>
-<footer class="footer mt-auto py-3">
+<footer class="footer mt-auto py-3 w-100">
     <div class="container">
         <span class="text-muted">Place sticky footer content here.</span>
     </div>
