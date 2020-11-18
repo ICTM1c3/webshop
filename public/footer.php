609d0b4a0486b9e47768b1c22f4b4803144c7c7f
sebastiaan geleedst <beerbootking@gmail.com>
2020-11-18 16:10:56 +0100

    footer aangepast

--- public/footer.php
+++ public/footer.php
@@ -1,6 +1,9 @@
 </div>
-<footer class="footer">
-</footer>
 </div>
+<footer class="footer mt-auto py-3">
+    <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+    </div>
+</footer>
 </body>
 </html>
