4898599fdb7a7efde61cfaad3a19a5d4686a534c
sebastiaan geleedst <beerbootking@gmail.com>
2020-11-18 16:48:52 +0100

    footer gevuld, help ward

--- public/footer.php
+++ public/footer.php
@@ -2,7 +2,32 @@
 </div>
 <footer class="footer mt-auto py-3 w-100">
     <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
+        <table class="footertable">
+        <tr></tr>
+        <th><span class="text-muted">Ut et volutpat ex, et porta dol. </span> </th>
+        <th></th>
+        <th><span class="text-muted">consequat consectetur elit. Sed eu nulla eget. </span> </th>
+        <th></th>
+        <th><span class="text-muted">Lorem ipsum dolor sit amet. </span> </th>
+        <th></th>
+            <th><span class="text-muted">Ut et volutpat ex</span> </th>
+        <tr></tr>
+            <th><span class="text-muted">Ut et volutpat ex, et porta dol. </span> </th>
+            <th></th>
+            <th><span class="text-muted">consequat consectetur elit. Sed eu nulla eget. </span> </th>
+            <th></th>
+            <th><span class="text-muted">Lorem ipsum dolor sit amet. </span> </th>
+            <th></th>
+            <th><span class="text-muted">Ut et volutpat ex</span> </th>
+            <tr></tr>
+            <th><span class="text-muted">Ut et volutpat ex, et porta dol. </span> </th>
+            <th></th>
+            <th><span class="text-muted">consequat consectetur elit. Sed eu nulla eget. </span> </th>
+            <th></th>
+            <th><span class="text-muted">Lorem ipsum dolor sit amet. </span> </th>
+            <th></th>
+            <th><span class="text-muted">Ut et volutpat ex</span> </th>
+        </table>
     </div>
 </footer>
 </body>
