80524c651cb076637440815f212f3994804b74c6
sebastiaan geleedst <beerbootking@gmail.com>
2020-11-25 14:36:20 +0100

    kortings code veld toegevoegd.

--- public/shopping-cart.php
+++ public/shopping-cart.php
@@ -4,7 +4,7 @@ ob_start();
 $success_messages = [];
 $errors = [];
 
-if($_SERVER['REQUEST_METHOD'] === "POST") {
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
     include 'config.php';
 
     if (isset($_POST['product_id']) && !empty($_POST['product_id'])) {
@@ -34,7 +34,7 @@ if($_SERVER['REQUEST_METHOD'] === "POST") {
                 case "update":
                     // Toevoegen/bewerken van product aan/in winkelmand.
 
-                    if ((int) $amount === 0) {
+                    if ((int)$amount === 0) {
                         unset($_SESSION['shopping_cart'][$product_id]);
                         $success_messages[] = "Het product is verwijderd uit de winkelwagen.";
                         break;
@@ -71,82 +71,92 @@ if($_SERVER['REQUEST_METHOD'] === "POST") {
     <h1 class="mb-3">Winkelwagen</h1>
     <div>
         <?php
-        foreach($errors as $key => $value) {
+        foreach ($errors as $key => $value) {
             ?>
-            <div class="alert alert-danger"><?=$value?></div>
+            <div class="alert alert-danger"><?= $value ?></div>
             <?php
         }
 
-        foreach($success_messages as $key => $value) {
+        foreach ($success_messages as $key => $value) {
             ?>
-            <div class="alert alert-success"><?=$value?></div>
+            <div class="alert alert-success"><?= $value ?></div>
             <?php
         }
         ?>
     </div>
     <?php
     if (isset($_SESSION['shopping_cart']) && !empty($_SESSION['shopping_cart'])) {
-        $products = $_SESSION['shopping_cart'];
-        $totale_prijs = 0;
-        foreach ($products as $product) {
-            $totale_prijs += $product['Price'] * $product['amount'];
-            ?>
-            <div class="row">
-                <div class="col-sm-6 col-md-3">
-                    <img src="public/<?= isset($product['ImagePath']) ? "stockitemimg/" . $product['ImagePath'] : "stockgroupimg/" . $product['BackupImagePath'] ?>" alt="" class="img-fluid">
-                </div>
-                <div class="col-sm-6 col-md-7">
-                    <h3><a class="text-white" target="_blank" href="view.php?id=<?= $product['StockItemId'] ?>"><?= $product['StockItemName'] ?></a></h3>
-                    <p class="mb-1"><span>Artikelnummer: <?= $product['StockItemId'] ?></span></p>
-                    <p><span>Prijs: &euro;<?= number_format($product['Price'] * $product['amount'], 2, ',', '.') ?> (&euro;<?= number_format($product['Price'], 2, ',', '.') ?> per stuk)</span></p>
-                    <form method="POST" action="shopping-cart.php" class="mb-3">
-                        <input type="hidden" name="product_id" id="product_id" value="<?= $product['StockItemId'] ?>">
-                        <input type="hidden" name="action" value="update">
-                        <div class="form-row">
-                            <div class="col-sm-2">
-                                <input min="1" required type="number" name="amount" class="form-control" placeholder="Aantal" value="<?= $product['amount'] ?>">
-                            </div>
-                            <div class="col-sm-2">
-                                <button type="submit" class="btn btn-primary">Bijwerken</button>
-                            </div>
-                        </div>
-                    </form>
-                    <form method="POST" action="shopping-cart.php">
-                        <input type="hidden" name="product_id" id="product_id" value="<?= $product['StockItemId'] ?>">
-                        <input type="hidden" name="action" value="remove">
-                        <button type="submit" class="btn btn-danger">Verwijderen</button>
-                    </form>
-                </div>
-            </div>
-            <hr class="border-white"/>
-            <?php
-        }
+    $products = $_SESSION['shopping_cart'];
+    $totale_prijs = 0;
+    foreach ($products as $product) {
+        $totale_prijs += $product['Price'] * $product['amount'];
         ?>
-        <div>
-            <h3>Totale prijs: &euro;<?= number_format($totale_prijs, 2, ',', '.'); ?></h3>
-        </div>
-            <div class="row">
-            <div class="col-12">
-                <form class="card p-2" action="shopping-cart.php" method="post">
-                    <div class="input-group">
-                kortingscode: <input class="form-control" name="kortingscodeveld" value="" type="text">
-                <div class="input-group-append">
-                <button type="submit" class="btn btn-secondary" name="kortingscodeknop" value="ok"> test
-                    </button>
-                </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-3">
+                <img src="public/<?= isset($product['ImagePath']) ? "stockitemimg/" . $product['ImagePath'] : "stockgroupimg/" . $product['BackupImagePath'] ?>"
+                     alt="" class="img-fluid">
+            </div>
+            <div class="col-sm-6 col-md-7">
+                <h3><a class="text-white" target="_blank"
+                       href="view.php?id=<?= $product['StockItemId'] ?>"><?= $product['StockItemName'] ?></a></h3>
+                <p class="mb-1"><span>Artikelnummer: <?= $product['StockItemId'] ?></span></p>
+                <p>
+                    <span>Prijs: &euro;<?= number_format($product['Price'] * $product['amount'], 2, ',', '.') ?> (&euro;<?= number_format($product['Price'], 2, ',', '.') ?> per stuk)</span>
+                </p>
+                <form method="POST" action="shopping-cart.php" class="mb-3">
+                    <input type="hidden" name="product_id" id="product_id" value="<?= $product['StockItemId'] ?>">
+                    <input type="hidden" name="action" value="update">
+                    <div class="form-row">
+                        <div class="col-sm-2">
+                            <input min="1" required type="number" name="amount" class="form-control"
+                                   placeholder="Aantal" value="<?= $product['amount'] ?>">
+                        </div>
+                        <div class="col-sm-2">
+                            <button type="submit" class="btn btn-primary">Bijwerken</button>
+                        </div>
                     </div>
                 </form>
-            </div>
+                <form method="POST" action="shopping-cart.php">
+                    <input type="hidden" name="product_id" id="product_id" value="<?= $product['StockItemId'] ?>">
+                    <input type="hidden" name="action" value="remove">
+                    <button type="submit" class="btn btn-danger">Verwijderen</button>
+                </form>
             </div>
         </div>
-        <?php
-    } else {
-        ?>
-        <p class="mb-1">Er zitten geen producten in de winkelwagen.</p>
-        <p>Klik <a href="index.php">hier</a> om terug naar de homepagina te gaan.</p>
+        <hr class="border-white"/>
         <?php
     }
     ?>
+    <div>
+        <h3>Totale prijs: &euro;<?= number_format($totale_prijs, 2, ',', '.'); ?></h3>
+    </div>
+    <div class="row bg-dark">
+        <div class="col-12">
+            <form class="p-2" action="shopping-cart.php" method="post">
+                <div class="form-row">
+                    <div class="col-sm-12 col-md-4">
+                    <label for="kortingscodeveld">kortingscode:</label>
+                    <div class="input-group">
+                        <input class="form-control" name="kortingscodeveld" value="" type="text">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-secondary" name="kortingscodeknop" value="ok"> ok
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<?php
+} else {
+    ?>
+    <p class="mb-1">Er zitten geen producten in de winkelwagen.</p>
+    <p>Klik <a href="index.php">hier</a> om terug naar de homepagina te gaan.</p>
+    <?php
+}
+?>
 </div>
 <?php
 include 'footer.php';
