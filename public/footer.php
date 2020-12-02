62b35c34cb6577c776d978c800eaccda641228b4
Ward Pieters <ward@wardpieters.nl>
2020-12-03 00:16:20 +0100

    fix: footer

--- public/footer.php
+++ public/footer.php
@@ -2,9 +2,9 @@
 </main>
 <footer class="mt-auto py-3">
     <div class="text-center">
-        <p class="mb-0">Telefoonnummer: <a href="tel:+62896706255135">+31-1234-567789</a> |
-            E-mailadres: <a href="mailto:info@nerdygadgets.nl">Info@nerdygadgets.nl </a> |
-            Vestiging: <a href="https://goo.gl/maps/9SYMuuvUCx9zvexP9"> Campus 2, 8017CA Zwolle</a></p>
+        <p class="mb-0">Telefoonnummer: <u><a class="text-white" href="tel:31612345678">+31 6 12345678</a></u> |
+            E-mailadres: <u><a class="text-white" href="mailto:info@nerdygadgets.nl">Info@nerdygadgets.nl</a></u> |
+            Vestiging: <u><a class="text-white" href="https://goo.gl/maps/9SYMuuvUCx9zvexP9"> Campus 2, 8017CA Zwolle</a></u></p>
     </div>
     <div class="text-center">
         <p class="mb-0">&copy; Nerdy Gadgets <?= date('Y') ?></p>
