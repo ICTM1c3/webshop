12a8fe44af4b30c9f41d2dcd6e25d0cbe14b00a6
Wesley <s1161858@student.windesheim.nl>
2020-12-02 15:25:14 +0100

    Contact informatie toegevoegd aan footer

--- public/footer.php
+++ public/footer.php
@@ -2,6 +2,11 @@
 </main>
 <footer class="mt-auto py-3">
     <div class="text-center">
+        <p class="mb-0">Telefoonnummer: <a href="tel:+62896706255135">+31-1234-567789</a> |
+            E-mailadres: <a href="mailto:info@nerdygadgets.nl">Info@nerdygadgets.nl </a> |
+            Vestiging: <a href="https://goo.gl/maps/9SYMuuvUCx9zvexP9"> Campus 2, 8017CA Zwolle</a></p>
+    </div>
+    <div class="text-center">
         <p class="mb-0">&copy; Nerdy Gadgets <?= date('Y') ?></p>
     </div>
 </footer>
